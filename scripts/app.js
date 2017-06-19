3f0c21c2fac4748c4c82d97214cacb497a9657cb
Traci Williams <ug3000@gmail.com>
2017-06-18 19:07:23 -0500

    new files

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+window.onload = function welcome(){
+	alert("hello! Welcome to 'Make your Family Tree'! Let's get started!");
+};
+
+
+var yourname, momname, dadname, sistername, brothername;
+
+yourname=$("#start");
