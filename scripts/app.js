2a89863b8dd300f2e446bfc20b47c6e36dfe9dd4
Traci Williams <ug3000@gmail.com>
2017-06-18 20:49:27 -0500

    function working with multiple values

--- scripts/app.js
+++ scripts/app.js
@@ -17,15 +17,17 @@
 
 function addName() {
 	var you = document.getElementById("you").value;
-
-
-
-	//you = document.getElementById("yourName").innerHTML;
-
+	var mom = document.getElementById("mom").value;
+	var dad = document.getElementById("dad").value;
+	var sister = document.getElementById("sister").value;
+	var brother = document.getElementById("brother").value;
 	
-
 	var entry = document.createElement("li");
 	entry.appendChild(document.createTextNode(you));
+	entry.appendChild(document.createTextNode(mom));
+	entry.appendChild(document.createTextNode(dad));
+	entry.appendChild(document.createTextNode(sister));
+	entry.appendChild(document.createTextNode(brother));
 
 
 	
